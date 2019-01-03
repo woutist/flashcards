6f30a7e0fecf9a9b9a9a2f5fa3773bd329bb33f9
alexn <olek@wpgeeks.co.uk>
2019-01-03 07:49:59 +0100

    first commit

---
+++ www/precache-manifest.f536c89cc63ea29e6f6d0f247a84b66e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c66b03711a1665b8bfcd702d622fd791",
+    "url": "/static/media/fontello.c66b0371.svg"
+  },
+  {
+    "revision": "42bd76d23cde35bda2b5",
+    "url": "/static/css/main.bc8814c3.chunk.css"
+  },
+  {
+    "revision": "8852d272b1b9fbbad832",
+    "url": "/static/js/1.8852d272.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8eefaaa5f48f843cabe0cb2a52b86691",
+    "url": "/static/media/fontello.8eefaaa5.eot"
+  },
+  {
+    "revision": "42bd76d23cde35bda2b5",
+    "url": "/static/js/main.42bd76d2.chunk.js"
+  },
+  {
+    "revision": "76a08872786621ea74f44fe542408052",
+    "url": "/static/media/fontello.76a08872.woff2"
+  },
+  {
+    "revision": "b500cf03eb817f4e267245e85444d4eb",
+    "url": "/static/media/fontello.b500cf03.woff"
+  },
+  {
+    "revision": "06e5774296896e199348dc3fa252b325",
+    "url": "/static/media/fontello.06e57742.ttf"
+  },
+  {
+    "revision": "f5b339663f96f38e3b71f35cae0c4309",
+    "url": "/static/media/bg3.f5b33966.jpg"
+  },
+  {
+    "revision": "6cbf8508cacf4bb3fa476e8cfae613c6",
+    "url": "/index.html"
+  }
+];
